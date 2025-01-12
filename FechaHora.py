ed09f43208446908b93d67d237b250efc944865f
Sebastián Martínez <sebmartinezse@unal.edu.co>
2025-01-12 11:13:03 -0500

    Cambios en la clase FechaHora

--- FechaHora.py
+++ FechaHora.py
@@ -1,42 +1,16 @@
 from Fecha import *
 from Hora import *
 
-class FechaHora(Fecha, Hora):
-    def __init__(self, dd, mm, aa, hh, nn, ss):
-        super().__init__(dd, mm, aa, hh, nn, ss)
-    
-    def getDia(self):
-        return self.__dia
-    
-    def setDia(self, dd):
-        self.__dia = dd
-    
-    def getMes(self):
-        return self.__mes
+class FechaHora:
+    def __init__(self, fecha: Fecha, hora: Hora):
+        self.__fecha = fecha  # Instancia de la clase Fecha
+        self.__hora = hora    # Instancia de la clase Hora
 
-    def setMes(self, mm):
-        self.__mes = mm
-    
-    def getAño(self):
-        return self.__año
-    
-    def setAño(self, aa):
-        self.__año = aa
-    
+    def getFecha(self):
+        return self.__fecha
+
     def getHora(self):
         return self.__hora
-    
-    def getMinuto(self):
-        return self.__minuto
-    
-    def getSegundo(self):
-        return self.__segundo
-    
-    def setHora(self, hh):
-        self.__hora = hh
-        
-    def setMinuto(self, nn):
-        self.__minuto = nn
-        
-    def setSegundo(self, ss):
-        self.__segundo = ss
+
+    def __str__(self):
+        return f"{self.__fecha} {self.__hora}"
