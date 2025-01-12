718b91a0284629afbc7ec708ea2b13922205c817
Sebastián Martínez <sebmartinezse@unal.edu.co>
2025-01-11 22:48:40 -0500

    Correción

--- Equipo.py
+++ Equipo.py
@@ -1,17 +1,18 @@
 from Usuario import Usuario
 from Fecha import Fecha
+
 class Equipo():
     def __init__(self, nombre, numeroPlaca, valorCompra):
         self.__nombre = nombre
         self.__numeroPlaca = numeroPlaca
         self.__fechaCompra = Fecha
         self.__valorCompra = valorCompra
-        self.__EmpAsociado = Usuario
+        self.__empAsociado = Usuario
     
     def setNombre(self, n):
         self.__nombre = n
     
-    def setNumeroPlaca(self, np):
+    def setNumeroPlaca(self, np): 
         self.__numeroPlaca = np
     
     def setFechaCompra(self, Fecha):
@@ -21,7 +22,7 @@ class Equipo():
         self.__valorCompra = vc
         
     def setEmpAsociado(self, Usuario):
-        self.__EmpAsociado = Usuario
+        self.__empAsociado = Usuario
         
     def getNombre(self):
         return self.__nombre
@@ -36,4 +37,4 @@ class Equipo():
         return self.__valorCompra
     
     def getEmpAsociado(self):
-        return self.__EmpAsociado
+        return self.__empAsociado
