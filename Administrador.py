091c23ef8930523049b7b42814cb288b0a5f5e0f
Richi <rfuentesa@unal.edu.co>
2025-01-14 12:50:15 -0500

    Admin en main y sus opciones (WIP)

--- Administrador.py
+++ Administrador.py
@@ -5,11 +5,34 @@ from Solicitud import *
 from Investigador import *
 
 class Administrador(Usuario):
-    def __init__(self, nombre, id, fechaNacimiento, ciudadNacimiento, telefono, email, dir, contraseña, inventario = None):
-        super().__init__(nombre, id, fechaNacimiento, ciudadNacimiento,telefono, email, dir)
+    def __init__(self, nombre, id, ciudadNacimiento, telefono, email, contraseña, inventario = None):
+        """super().__init__(nombre, id, fechaNacimiento, ciudadNacimiento,telefono, email, dir)"""
+        self.__nombre = nombre
+        self.__id = id
+        self.__fechaNacimiento = Fecha
+        self.__ciudadNacimiento = ciudadNacimiento
+        self.__telefono = telefono
+        self.__email = email
+        self.__dir = Direccion
         self.__contraseña = contraseña
         self.__inventario = inventario if inventario is not None else []
     
+    def setInventario(self, inventario):
+        self.__inventario = inventario
+    
+    def getInventario(self):
+        return self.__inventario
+    
     """Registrar Nuevos usuarios, cambiar contraseñas, eliminar usuarios, Solicitudes de nuevo equipo, Lo mismo para eliminar equipo"""
     """Generar txt segun el investigador y txt de todos los investigadores ordenado de menor a mayor segun la placa"""
-    """txt´s de control de cambios, solicitudes agregar y eliminar"""   
+    """txt´s de control de cambios, solicitudes agregar y eliminar"""  
+    
+    def generarInventario(self, lista, nombreArchivo):
+        with open(nombreArchivo, "w") as archivo:
+            for elemento in lista:
+                elemento = elemento.__str__()
+                archivo.write(f"{elemento}\n")
+        print(f"Lista guardada correctamente como: {nombreArchivo}")
+        
+    def __str__(self):
+                return "Objeto creado :)"
--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from Investigador import *;from Direccion import *; from Equipo import *;from FechaHora import *
 from Fecha import *; from Hora import *; from List import *; from ListaDoble import *;from Nodo import *; from NodoDoble import*
-from Solicitud import *;from Usuario import *
+from Solicitud import *;from Usuario import *; from Administrador import *
 
 #Cargue de empleados
 emp = List(None,None,0)
@@ -48,18 +48,48 @@ def OpcionesInvestigador():
     if op == 1:
         n = IV.getNombre()
         id = str(IV.getId())
-        IV.generarInventario(IV.generarInventario, n + id)
+        IV.generarInventario(IV.getInventario(), n + " " + id)
     elif op == 2:
         pass
     elif op == 3:
         pass
     elif op == 4:
         pass
+        
     else:
         print("Opcion incorrecta")
 
+def OpcionesAdministrador():
+    print("***********MENU***********")
+    print("1.txt inventario")
+    print("2.Agregar usuario")
+    print("3.Eliminar usuario")
+    print("4.Cambiar contraseña")
+    print("5.Solicitudes Agregar equipo")
+    print("6.Solicitudes Eliminar equipo")
+    print("7.Inventario segun investigador(txt)")
+    print("8.Inventario general(txt)")
+    print("9.Control de cambios(txt)")
+    print("10.Solicitudes agregar(txt)")
+    print("11.Solicitudes eliminar(txt)")
+    op = int(input())
+    if op == 1:
+        n = AD.getNombre()
+        id = str(AD.getId())
+        AD.generarInventario(AD.getInventario(), n + " " + id)
+    elif op == 2:
+        pass
+    elif op == 3:
+        pass
+    elif op == 4:
+        pass
+        
+    else:
+        print("Opcion incorrecta")
+    
 #apuntadores para la sesion iniciada, investigador o admin
 IV = None
+AD = None
 
 #Ciclo para verificar id y contraseña ingresados vf = verificacion
 temp = pss.First()
@@ -111,12 +141,53 @@ while temp != None and temp.getNext() != None:
                     IV.setDir(Direccion(calle,nomn,brr,ciudad,edfc,apto))
                     print("empleado encontrado", IV)
                     OpcionesInvestigador()
-                    
+        
+        #en caso de Administrador relaciono id de la contraseña con id en la lista de empleados
+        if temp.getData()[2] == "administrador":
+            temp1 = emp.First()
+            while temp1 != None and temp1.getNext() != None:
+                #Creacion objeto administrador
+                if temp.getData()[0] == temp1.getData()[1]:
+                    AD = Administrador(str(temp1.getData()[0]),int(temp1.getData()[1]),None,str(temp1.getData()[3]),int(temp1.getData()[4]),str(temp1.getData()[5]),None,)
+                    j = temp1.getData()[2].split()
+                    dd = j[0]
+                    mm = j[1]
+                    aa = j[2]
+                    AD.setFechaNacimiento(Fecha(dd,mm,aa))
+                    k = temp1.getData()[6].split()
+                    calle = k[0]
+                    nomn = k[1]
+                    brr = k[2]
+                    ciudad = k[3]
+                    edfc = k[4]
+                    apto = k[5]
+                    AD.setDir(Direccion(calle,nomn,brr,ciudad,edfc,apto))
+                    print("Admin encontrado", AD)
+                temp1 = temp1.getNext()
+            #tail lista empleados
+            if temp.getData()[0] == temp1.getData()[1]:
+                    AD = Administrador(str(temp1.getData()[0]),int(temp1.getData()[1]),None,str(temp1.getData()[3]),int(temp1.getData()[4]),str(temp1.getData()[5]),None,)
+                    j = temp1.getData()[2].split()
+                    dd = j[0]
+                    mm = j[1]
+                    aa = j[2]
+                    AD.setFechaNacimiento(Fecha(dd,mm,aa))
+                    k = temp1.getData()[6].split()
+                    calle = k[0]
+                    nomn = k[1]
+                    brr = k[2]
+                    ciudad = k[3]
+                    edfc = k[4]
+                    apto = k[5]
+                    AD.setDir(Direccion(calle,nomn,brr,ciudad,edfc,apto))
+                    print("Admin encontrado", AD)
     temp = temp.getNext()
     
 #este es el tail de la lista de contraseña
 if temp.getData()[0] == NumId and temp.getData()[1] == contra:
     vf = True
+    
+    #en caso de investigador 
     if temp.getData()[2] == "investigador":
             temp1 = emp.First()
             while temp1 != None and temp1.getNext() != None:
@@ -161,6 +232,45 @@ if temp.getData()[0] == NumId and temp.getData()[1] == contra:
                     print("empleado encontrado", IV)
                     OpcionesInvestigador()
     
+    #en caso de Administrador relaciono id de la contraseña con id en la lista de empleados
+    if temp.getData()[2] == "administrador":
+        temp1 = emp.First()
+        while temp1 != None and temp1.getNext() != None:
+            #Creacion objeto administrador
+            if temp.getData()[0] == temp1.getData()[1]:
+                AD = Administrador(str(temp1.getData()[0]),int(temp1.getData()[1]),None,str(temp1.getData()[3]),int(temp1.getData()[4]),str(temp1.getData()[5]),None,)
+                j = temp1.getData()[2].split()
+                dd = j[0]
+                mm = j[1]
+                aa = j[2]
+                AD.setFechaNacimiento(Fecha(dd,mm,aa))
+                k = temp1.getData()[6].split()
+                calle = k[0]
+                nomn = k[1]
+                brr = k[2]
+                ciudad = k[3]
+                edfc = k[4]
+                apto = k[5]
+                AD.setDir(Direccion(calle,nomn,brr,ciudad,edfc,apto))
+                print("Admin encontrado", AD)
+            temp1 = temp1.getNext()
+        #tail lista empleados
+        if temp.getData()[0] == temp1.getData()[1]:
+                AD = Administrador(str(temp1.getData()[0]),int(temp1.getData()[1]),None,str(temp1.getData()[3]),int(temp1.getData()[4]),str(temp1.getData()[5]),None,)
+                j = temp1.getData()[2].split()
+                dd = j[0]
+                mm = j[1]
+                aa = j[2]
+                AD.setFechaNacimiento(Fecha(dd,mm,aa))
+                k = temp1.getData()[6].split()
+                calle = k[0]
+                nomn = k[1]
+                brr = k[2]
+                ciudad = k[3]
+                edfc = k[4]
+                apto = k[5]
+                AD.setDir(Direccion(calle,nomn,brr,ciudad,edfc,apto))
+                print("Admin encontrado", AD)
 
 if vf == False: 
     print("Acceso incorrecto")
